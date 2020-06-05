4e4cf00a31290513f824aebcd8a42054000771eb
Eli Landau <eli.b.landau@gmail.com>
2020-06-05 15:03:09 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,5 @@
 def find_max_value(array)
-  counter = 0
-  new_array = []
-  
-  while counter < array.length do
-    new_array.push(array.sort)
-    counter += 1
-  end  
-  new_array.shift()
+ new_array = array.sort
+ 
+  new_array.pop()
 end
